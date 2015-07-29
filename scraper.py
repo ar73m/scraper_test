56100f1e93d3616113259756cc60c0992e081497
Artem <ar73m@yandex.ru>
2015-07-29 18:36:09 +0300

    Test

--- scraper.py
+++ scraper.py
@@ -1,54 +1,31 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
-
+# -*- coding: utf-8 -*-
 import scraperwiki
 import lxml.html
-#
-# # Read in a page
+
 print("Get HTML")
 html = scraperwiki.scrape("http://lenta.ru/")
-#
-# # Find something on the page using css selectors
+
 root = lxml.html.fromstring(html)
 a = root.xpath('.//div[@class="items"]//div[@class="item"]')
+
 for r in a:
-    print("{} -- {}".format(r.xpath('./time/@datetime')[0].encode('utf-8'), 
-    r.xpath('./a/text()')[0].encode('utf-8')
-    ))
-    print("http://lenta.ru{}"
-    .format(
-    r.xpath('./a/@href')[0].encode('utf-8')))
+    date = r.xpath('./time/@datetime')[0]
+    text = r.xpath('./a/text()')[0]
+    url =  r.xpath('./a/@href')[0]
+    
+    print("{} -- {}".format(date.encode('utf-8'), text.encode('utf-8')))
+    print("http://lenta.ru{}".format(url.encode('utf-8')))
     
     scraperwiki.sqlite.save(
     unique_keys=[],
-    data={"time": r.xpath('./time/@datetime')[0].encode('utf-8'),
-          "text": r.xpath('./a/text()')[0].encode('utf-8'),
-          "url": "http://lenta.ru{}".format(r.xpath('./a/@href')[0].encode('utf-8'))
-        }
-)
-# .encode('cp866')
+    data={"time": date,
+          "text": text,
+          "url": u"http://lenta.ru{}".format(url)
+        })
+        
+        
+        
+# .encode('cp866') decode('cp1251')
 
 
 
