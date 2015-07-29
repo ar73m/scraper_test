81fe6781b723d349ae0632d55997f47e32eebe35
Artem <ar73m@yandex.ru>
2015-07-29 14:54:45 +0300

    First

--- scraper.py
+++ scraper.py
@@ -22,3 +22,27 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+import scraperwiki
+import lxml.html
+#
+# # Read in a page
+html = scraperwiki.scrape("http://m.vk.com")
+#
+# # Find something on the page using css selectors
+root = lxml.html.fromstring(html)
+a = root.path('//section/a/@href')
+for r in a:
+    print r
+#
+# # Write out to the sqlite database using scraperwiki library
+#scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+#
+# # An arbitrary query against the database
+#scraperwiki.sql.select("* from data where 'name'='peter'")
+
+# You don't have to do things with the ScraperWiki and lxml libraries.
+# You can use whatever libraries you want: https://morph.io/documentation/python
+# All that matters is that your final data is written to an SQLite database
+# called "data.sqlite" in the current working directory which has at least a table
+# called "data".
